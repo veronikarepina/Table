682048d7b22f60651efbb90df4eed952ea87a1fc
veronikarepina <r.v.99.v.r@gmail.com>
2023-03-28 20:34:04 +0300

    refactor

--- app/src/main/java/ru/veronikarepina/table/adapter/TableAdapter.kt
+++ app/src/main/java/ru/veronikarepina/table/adapter/TableAdapter.kt
@@ -1,7 +1,6 @@
 package ru.veronikarepina.table.adapter
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
@@ -10,28 +9,15 @@ import ru.veronikarepina.table.R
 import ru.veronikarepina.table.model.Participant
 
 
-class Adapter(private val context: Context, private val listener: Listener): ListAdapter<Participant, TableNewsHolder>(DiffUtilCallBack()) {
-
+class TableAdapter(private val context: Context, private val listener: Listener): ListAdapter<Participant, TableNewsHolder>(DiffUtilCallBack()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TableNewsHolder {
-        Log.d("MyLog", "ON CREATE VIEW HOLDER")
         val view = LayoutInflater.from(parent.context).inflate(R.layout.table_row, parent, false)
         return TableNewsHolder(view)
     }
-
     override fun onBindViewHolder(holder: TableNewsHolder, position: Int) {
-        Log.d("MyLog", "ON BIND VIEW HOLDER")
         holder.bind(getItem(position), context, listener, position)
     }
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
 }
-
-//inline fun EditText.onTextChange(crossinline listener: (String) -> Unit) {
-//    this.addTextChangedListener(object: TextWatcher {
-//        override fun beforeTextChanged(charSequence: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-//
-//        override fun onTextChanged(charSequence: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//            listener(charSequence.toString())
-//        }
-//
-//        override fun afterTextChanged(p0: Editable?) {}
-//    })
-//}
--- app/src/main/java/ru/veronikarepina/table/presentation/TableViewModel.kt
+++ app/src/main/java/ru/veronikarepina/table/presentation/TableViewModel.kt
@@ -1,7 +1,6 @@
 package ru.veronikarepina.table.presentation
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -9,74 +8,103 @@ import ru.veronikarepina.table.R
 import ru.veronikarepina.table.model.Participant
 
 class TableViewModel(application: Application): AndroidViewModel(application) {
-    private val _listLiveData = MutableLiveData<ArrayList<Participant>>()
-    val listLiveData: LiveData<ArrayList<Participant>> = _listLiveData
-    private val participantList = ArrayList<Participant>()
+    private val _listLiveData = MutableLiveData<MutableList<Participant>>()
+    val listLiveData: LiveData<MutableList<Participant>> = _listLiveData
+    private var participantList: MutableList<Participant> = mutableListOf()
+    private val bufferList: MutableList<Participant>
 
     init {
-        Log.d("MyLog", "INIT")
         initList(application)
+        bufferList = participantList.map { it.copy() }.toMutableList()
         _listLiveData.value = participantList
     }
 
-    fun setValues(value: String, position: Int, field: Int){
+    fun setValues(value: String, position: Int, field: Int) = with(bufferList[position]){
         when(field){
-            1 -> participantList[position].field1 = value
-            2 -> participantList[position].field2 = value
-            3 -> participantList[position].field3 = value
-            4 -> participantList[position].field4 = value
-            5 -> participantList[position].field5 = value
-            6 -> participantList[position].field6 = value
-            7 -> participantList[position].field7 = value
+            1 -> field1 = value
+            2 -> field2 = value
+            3 -> field3 = value
+            4 -> field4 = value
+            5 -> field5 = value
+            6 -> field6 = value
+            7 -> field7 = value
         }
-        Log.d("MyLog", "LIST IN MODEL: $participantList")
-//        if(participantList[position].field2.isNotEmpty()){
-//            Log.d("MyLog", "NOT EMPTY")
-//            participantList[position].sum = participantList[position].field2
-//            Log.d("MyLog", "LIST IN MODEL: $participantList")
-////            Log.d("MyLog", "d: $d")
-//        }
 
-        if (participantList[position].field1.isNotEmpty() && participantList[position].field2.isNotEmpty() &&
-            participantList[position].field3.isNotEmpty() && participantList[position].field4.isNotEmpty() &&
-            participantList[position].field5.isNotEmpty() && participantList[position].field6.isNotEmpty() &&
-            participantList[position].field7.isNotEmpty()){
-            participantList[position].sum = (participantList[position].field1.toInt() +
-                    participantList[position].field2.toInt() + participantList[position].field3.toInt() +
-                    participantList[position].field4.toInt() + participantList[position].field5.toInt() +
-                    participantList[position].field6.toInt() + participantList[position].field7.toInt()).toString()
-            Log.d("MyLog", "LIST IN MODEL: $participantList")
-            _listLiveData.value = participantList
-            Log.d("MyLog", "LIVE DATA: ${listLiveData.value}")
+        setSum(position)
+        setPositions()
+
+        participantList = bufferList.map { it.copy() }.toMutableList()
+        _listLiveData.value = participantList
+    }
+    private fun setSum(position: Int){
+        val listFields = with(bufferList[position]) {
+            listOf(
+                field1.toString(),
+                field2.toString(),
+                field3.toString(),
+                field4.toString(),
+                field5.toString(),
+                field6.toString(),
+                field7.toString()
+            )
+        }
+
+        bufferList[position].sum = if(!listFields.contains(""))
+            if (!listFields
+                    .filter { it!="null" }
+                    .map { it.toInt() in 0..5 }
+                    .contains(false)
+            )
+                listFields
+                    .filter { it != "null" }
+                    .sumOf { item -> item.toInt() }
+                    .toString()
+            else ""
+        else ""
+        if (bufferList[position].sum == "")
+            removePositions()
+    }
+    private fun setPositions(){
+        val listSums = mutableListOf<String>()
+        bufferList.map { listSums.add(it.sum) }
+
+        if (!listSums.contains("")){
+            val listMaps: MutableMap<Int, String> = mutableMapOf()
+            for ((index, item) in listSums.withIndex())
+                listMaps[index] = item
+
+            val sortedList = listSums
+                .map { it.toInt() }
+                .distinct()
+                .sortedDescending()
+                .map { it.toString() }
+
+            listMaps.forEach{
+                listMaps[it.key] = (sortedList.indexOf(it.value) + 1).toString()
+                bufferList[it.key].position = it.value
+            }
         }
     }
-    private fun sum(){
-
+    private fun removePositions(){
+        bufferList.map { it.position = "" }
     }
-    private fun positions(){
-
-    }
-    private fun initList(application: Application){
-        participantList.add(Participant(application.getString(R.string.participant1), application.getString(
-            R.string.one
-        ), "0", "", "", "", "", "", "", "", ""))
-        participantList.add(Participant(application.getString(R.string.participant2), application.getString(
-            R.string.two
-        ), "", "0", "", "", "", "", "", "", ""))
-        participantList.add(Participant(application.getString(R.string.participant3), application.getString(
+    private fun initList(application: Application) = with(participantList){
+        add(Participant(application.getString(R.string.participant1), application.getString(R.string.one), null, "", "", "", "", "", "", "", ""))
+        add(Participant(application.getString(R.string.participant2), application.getString(R.string.two), "", null, "", "", "", "", "", "", ""))
+        add(Participant(application.getString(R.string.participant3), application.getString(
             R.string.three
-        ), "", "", "0", "", "", "", "", "", ""))
-        participantList.add(Participant(application.getString(R.string.participant4), application.getString(
+        ), "", "", null, "", "", "", "", "", ""))
+        add(Participant(application.getString(R.string.participant4), application.getString(
             R.string.four
-        ), "", "", "", "0", "", "", "", "", ""))
-        participantList.add(Participant(application.getString(R.string.participant5), application.getString(
+        ), "", "", "", null, "", "", "", "", ""))
+        add(Participant(application.getString(R.string.participant5), application.getString(
             R.string.five
-        ), "", "", "", "", "0", "", "", "", ""))
-        participantList.add(Participant(application.getString(R.string.participant6), application.getString(
+        ), "", "", "", "", null, "", "", "", ""))
+        add(Participant(application.getString(R.string.participant6), application.getString(
             R.string.six
-        ), "", "", "", "", "", "0", "", "", ""))
-        participantList.add(Participant(application.getString(R.string.participant7), application.getString(
+        ), "", "", "", "", "", null, "", "", ""))
+        add(Participant(application.getString(R.string.participant7), application.getString(
             R.string.seven
-        ), "", "", "", "", "", "", "0", "", ""))
+        ), "", "", "", "", "", "", null, "", ""))
     }
 }
